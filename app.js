805752be6ce7d388e075515d2e55931f430b2065
Abhishek Gawade <78852210+8291035406@users.noreply.github.com>
2021-12-30 15:20:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const weatherApi = {
+    key:'bab281d79e5f1e9755a68d754cc313e7',
+    baseUrl:'https://api.openweathermap.org/data/2.5/weather?',
+}
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const  searchInputBox = document.getElementById('input-box');
+// Event Listener Function on keypress
+searchInputBox.addEventListener('keypress',(Event)=>{
+    if(Event.keyCode == 13)
+    {
+        console.log(searchInputBox.value);
+        getWeatherReport(searchInputBox.value);
+    }
+});
+//Get weatherreport
+function getWeatherReport(city){
+    fetch('${weatherApi.baseUrl}?q=${city}&appid={bab281d79e5f1e9755a68d754cc313e7}')
+    .then(weather =>{
+        return weather.json(showWeatherReport);
+    }).then();
+}
+//Show wewther report
+function showWeatherReport(weather){
+    console.log(weather);
+}
+//Date manager
